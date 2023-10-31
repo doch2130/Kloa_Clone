1576261fff96eecbe2930714f3f6e5e1a1680dd9
doch2130 <doch2130@gmail.com>
2023-10-31 15:54:20 +0900

    feat: manager page calendar function api adding

--- frontend/src/app/manager/page.tsx
+++ frontend/src/app/manager/page.tsx
@@ -12,13 +12,34 @@ export default function Manager() {
         <span>데이터 갱신하기</span>
         <div className={styled.managerButtonGroup}>
           <button type='button' className={styled.managerNotice} onClick={() => getNoticeList()}>공지사항</button>
-          <button type='button' className={styled.managerSchedule}>스케줄</button>
+          <button type='button' className={styled.managerSchedule} onClick={() => getCalendarList()}>스케줄</button>
         </div>
       </div>
     </div>
   )
 }
 
+const getCalendarList = () => {
+  fetch('/api/calendar', {
+    method: 'GET'
+  })
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error('Network response was not ok');
+      }
+    })
+    .then((data) => {
+      // 데이터를 사용하는 코드를 여기에 작성
+      console.log('Data from API:', data);
+      alert('데이터 갱신이 완료되었습니다.');
+    })
+    .catch((error) => {
+      console.error('Fetch error: ', error);
+      alert('데이터 갱신 중 에러가 발생하였습니다. 잠시 후 다시 시도 해주세요.');
+    });
+}
 
 const getNoticeList = () => {
   fetch('/api/notices', {
