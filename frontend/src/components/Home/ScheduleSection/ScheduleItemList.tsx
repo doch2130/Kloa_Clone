93fcedc6976a3c14031868c4a0f3bd7bb729f262
doch2130 <doch2130@gmail.com>
2023-11-02 14:16:06 +0900

    feat: schedule island item alt text view adding

--- frontend/src/components/Home/ScheduleSection/ScheduleItemList.tsx
+++ frontend/src/components/Home/ScheduleSection/ScheduleItemList.tsx
@@ -4,7 +4,8 @@ import Image from 'next/image';
 
 import LeftDoubleArrow from '@/assets/Icon/leftDoubleArrow.svg'
 import RightDoubleArrow from '@/assets/Icon/rightDoubleArrow.svg'
-import itemFilter from '@/data/AdventureIslandData';
+import itemFilter from '@/data/AdventureIslandData'
+import styled from './ScheduleItemList.module.css'
 
 type ScheduleItemListProps = {
   islandName: string;
@@ -28,20 +29,35 @@ export default function ScheduleItemList(props:ScheduleItemListProps) {
     <>
       {itemList.map((el: rewardItem, index: number) => {
         const itemSrc = itemFilter(el.Name, islandName);
+        let iconText = el.Name;
+        if(el.Name === '전설 ~ 고급 카드 팩 III') {
+          iconText = '카드팩';
+        }
         return (
-        <div key={index}>
-          {index < 6 ? (arrowIndex && <Image src={itemSrc} alt={el.Name} width={30} height={30} />)
+        <div key={index} className={styled.itemIconWrap}>
+          {index < 6 ? (arrowIndex && 
+            <>
+              <Image src={itemSrc} alt={el.Name} width={30} height={30} className={styled.itemIcon} />
+              <p className={styled.itemIconText}>{iconText}</p>
+            </>)
           :
           index === 6 ? (
             arrowIndex ? (
-              <Image src={RightDoubleArrow} alt='Island Item List Right Arrow' width={30} height={30} onClick={itemListChange} />
+              <Image src={RightDoubleArrow} alt='Island Item List Right Arrow' width={30} height={30} onClick={itemListChange} className={styled.arrowIcon} />
             ) : (
               <>
-                <Image src={LeftDoubleArrow} alt='Island Item List Left Arrow' width={30} height={30} onClick={itemListChange} />
-                <Image src={itemSrc} alt={el.Name} width={30} height={30} />
+                <Image src={LeftDoubleArrow} alt='Island Item List Left Arrow' width={30} height={30} onClick={itemListChange} className={styled.arrowIcon} />
+                <Image src={itemSrc} alt={el.Name} width={30} height={30} className={styled.itemIcon} />
+                <p className={`${styled.itemIconText} ${styled.itemIconText2}`}>{iconText}</p>
               </>
             )
-          ) : index > 7 && (!arrowIndex && <Image src={itemSrc} alt={el.Name} width={30} height={30} />)}
+          ) : index >= 7 && (!arrowIndex && 
+          <>
+            <Image src={itemSrc} alt={el.Name} width={30} height={30} className={styled.itemIcon} />
+            <p className={styled.itemIconText}>{iconText}</p>
+          </>
+          )
+          }
         </div>
       )
       })}
