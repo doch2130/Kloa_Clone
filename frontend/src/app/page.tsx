2ff2272aeac4a0eaa63ae669b58700703d9a763c
HyoHyun_Home <doch2130@gmail.com>
2023-11-07 16:19:39 +0900

    Feat: Main Page Notices View Modify

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -30,6 +30,28 @@ export default async function Home() {
   const noticesList = await noticesResp.json();
   const noticesTopList = noticesList[0];
 
+  const mainNoticesResp = await fetch('http://localhost:9999/mainNotices',
+  {
+    cache: 'force-cache',
+    headers: {
+      // 캐시 유효 시간을 1시간으로 설정
+      'Cache-Control': 'max-age=3600',
+    }
+  });
+  type mainNoticeType = {
+    category: string,
+    title: string,
+    textData: string,
+    writeTime: string,
+    viewCount: number,
+    likeCount: number,
+    id: string,
+  }
+  const mainNoticesList:mainNoticeType[] = await mainNoticesResp.json();
+  mainNoticesList.sort((a, b) => Number(new Date(b.writeTime)) - Number(new Date(a.writeTime)));
+  const mainNoticesTopList = mainNoticesList.slice(0, 5);
+
+  
   const adventureResp = await fetch('http://localhost:9999/adventureIsland',
   {
     cache: 'force-cache',
@@ -71,11 +93,11 @@ export default async function Home() {
           </div>
           <div className={styled.noticeTable}>
             {
-              noticesTopList.map((el:noticesTopListType, index:number) => 
+              mainNoticesTopList.map((el:mainNoticeType, index:number) => 
               (
                 <div className={styled.noticeTableRow} key={index}>
-                  <div className={`${styled.noticeTableCategory} ${categoryClassMap[el.Type]}`}>{el.Type}</div>
-                  <Link href={el.Link} target="_blank">{el.Title}</Link>
+                  <div className={`${styled.noticeTableCategory} ${categoryClassMap[el.category]}`}>{el.category}</div>
+                  <Link href={`/notices/notices/detail/${el.id}`} target="_blank">{el.title}</Link>
                 </div>
               ))
             }
