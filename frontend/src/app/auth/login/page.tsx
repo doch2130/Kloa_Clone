1dd9beb105165b8c545ca9531ba1916434095715
doch2130 <doch2130@gmail.com>
2023-11-09 16:13:44 +0900

    feat: login function modify

--- frontend/src/app/auth/login/page.tsx
+++ frontend/src/app/auth/login/page.tsx
@@ -18,7 +18,7 @@ export default function Login() {
     setIsCheck(!isCheck);
   }
 
-  const onSubmit = (e:React.FormEvent) => {
+  const onSubmit = async (e:React.FormEvent) => {
     e.stopPropagation();
 
     if(idInputRef.current === null || pwdInputRef.current === null) {
@@ -34,43 +34,32 @@ export default function Login() {
       return ;
     }
 
-    fetch('/api/auth/login', {
-      method: 'POST',
-      body: JSON.stringify({
-        id: idInputRef.current?.value,
-        pwd: pwdInputRef.current?.value
-      })
-    })
-      .then((response) => {
-        // console.log('response ', response);
-        if (response.status === 401) {
-          alert('사용자 정보가 일치하지 않습니다.');
-          return false;
-        }
-        if (response.ok) {
-          return response.json();
-        } else {
-          throw new Error('Network response was not ok');
-        }
-      })
-      .then(async (data) => {
-        // 데이터를 사용하는 코드를 여기에 작성
-        // console.log('data ', data);
-        if(data !== false) {
-          const result = await signIn('credentials', {
-            email: idInputRef.current?.value,
-            redirect: false,
-          });
-          // console.log('result ', result);
-          alert('로그인 성공');
-          router.push('/');
-        }
-      })
-      .catch((error) => {
-        // console.error('Fetch error: ', error);
-        alert('로그인 시도에 실패하였습니다. 잠시 후 다시 시도해주세요.');
+    try {
+      const response = await signIn('credentials', {
+        email: idInputRef.current?.value,
+        pwd: pwdInputRef.current?.value,
+        redirect: false,
       });
+      // console.log('response ', response);
+
+      if(response) {
+        if(response.ok) {
+          alert('어서오세요!!');
+          router.push('/');
+        } else {
+          alert('로그인 정보가 일치하지 않습니다.');
+        }
+        return ;
+      } else {
+        alert('로그인 시도에 실패하였습니다. 새로고침 후 다시 시도해주세요.');
+        return ;
+      }
+    } catch (error) {
+      // console.error('Fetch error: ', error);
+      alert('로그인 시도에 실패하였습니다. 잠시 후 다시 시도해주세요.');
     }
+  }
+
   return (
     <form className={styled.loginForm} onSubmit={onSubmit}>
       <div className={styled.loginFormInputWrap}>
