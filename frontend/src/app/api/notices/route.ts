de094782284f5c4543f484a6656e841923424ac6
doch2130 <doch2130@gmail.com>
2023-11-10 18:02:38 +0900

    Feat: notice all & detail get Function Add, view Count Patch Modify

--- frontend/src/app/api/notices/route.ts
+++ frontend/src/app/api/notices/route.ts
@@ -1,8 +1,34 @@
 import { NextRequest, NextResponse } from "next/server";
 import prisma from '@/app/lib/prisma'
 
-export async function GET() {
+// 상세 페이지 가져오기 전용 함수
+async function getPostDetail(id:number) {
+  const post = await prisma.mainnotices.findFirst({
+    where: {
+      id: id
+    }
+  });
+  return post;
+}
+
+// 전체 리스트 가져오기, 상세 페이지 가져오기
+export async function GET(req: NextRequest) {
+  const { searchParams } = new URL(req.url);
+  const id = searchParams.get('id');
+  const detail = searchParams.get('detail');
+
   try {
+    // 상세 페이지 가져오는 함수 / 분기점
+    if(detail === 'true' && id !== null) {
+      const post = await getPostDetail(Number(id));
+      if(post) {
+        return new NextResponse(JSON.stringify({ success: true, status: 200, result: post }));
+      } else {
+        return new NextResponse(JSON.stringify({ success: true, status: 404, result: [] }));
+      }
+    }
+
+    // 전체 리스트 가져오는 함수
     // skip은 앞에서부터 X개를 건너뛰고, take는 건너뛴 이후 부터 X개를 가져온다는 의미
     const postList = await prisma.mainnotices.findMany({
       skip: 0,
@@ -27,12 +53,15 @@ export async function GET() {
 }
 
 
+// 조회수 증가 함수
 export async function PATCH(req: NextRequest) {
   const body = await req.json();
   // console.log('body ', body);
   try {
     const post = await prisma.mainnotices.findFirst({
-      where: body.id
+      where: {
+        id: body.id
+      }
     });
 
     if(!post) {
@@ -45,7 +74,9 @@ export async function PATCH(req: NextRequest) {
       data: {
         viewCount: updateViewCount
       },
-      where: body.id
+      where: {
+        id: body.id
+      }
     });
 
     if(postUpdate) {
@@ -59,34 +90,3 @@ export async function PATCH(req: NextRequest) {
     return new NextResponse(JSON.stringify({ success: false, status: 500 }));
   }
 }
-
-
-// export async function GET() {
-//   const url = 'https://developer-lostark.game.onstove.com/news/notices';
-
-//   try {
-//     const response = await fetch(url, {
-//       method: 'GET',
-//       cache: 'no-cache',
-//       headers: {
-//         'accept': 'application/json',
-//         'authorization': `bearer ${process.env.NEXT_PUBLIC_LOSTARK_API}`
-//       },
-//     });
-    
-//     if (response.ok) {
-//       const data = await response.json();
-//       return new NextResponse(JSON.stringify(data), {
-//         status: response.status,
-//         headers: {
-//           'Content-Type': 'application/json',
-//         },
-//       });
-//     } else {
-//       return new NextResponse('Error fetching data', { status: response.status });
-//     }
-//   } catch (error) {
-//     console.error('Error:', error);
-//     return new NextResponse('An error occurred', { status: 500 });
-//   }
-// }
