ab1594119b80adc0e93cc986a4a5344bd86713c0
HyoHyun_Home <doch2130@gmail.com>
2023-11-11 04:38:57 +0900

    Feat: fetch headers authorization bearer delete

--- frontend/src/app/notices/write/page.tsx
+++ frontend/src/app/notices/write/page.tsx
@@ -44,7 +44,7 @@ export default function Write() {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
-          'Authorization' : `bearer ${session?.user?.accessToken}` || 'bearer '
+          'Authorization' : `${session?.user?.accessToken}` || ''
         },
         body: JSON.stringify({
           category: categoryData,
