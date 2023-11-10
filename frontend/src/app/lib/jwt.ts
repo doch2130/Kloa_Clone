067e1bbfc805b0e66de8bba00e3f87c3cf1bf7ab
HyoHyun_Home <doch2130@gmail.com>
2023-11-11 05:18:36 +0900

    feat: jose temp create, not test

--- frontend/src/app/lib/jwt.ts
+++ frontend/src/app/lib/jwt.ts
@@ -1,4 +1,5 @@
-import jwt, { JwtPayload } from "jsonwebtoken";
+import jwt, { JwtPayload } from "jsonwebtoken"
+import {SignJWT, jwtVerify, type JWTPayload} from 'jose';
 
 interface SignOption {
   expiresIn?: string | number;
@@ -21,6 +22,40 @@ export function verifyJwt(token: string) {
     const secret_key = process.env.SECRET_KEY;
     const decoded = jwt.verify(token, secret_key!);
     return decoded as JwtPayload;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+}
+
+
+
+// jose 임시 작성 함수 = 테스트 예정
+export async function sign(payload: JwtPayload):Promise<string> {
+  try{
+    const secret_key = process.env.SECRET_KEY;
+    const iat = Math.floor(Date.now() / 1000);
+    const exp = iat + 60* 60; // one hour
+
+    const token = new SignJWT({...payload})
+        .setProtectedHeader({alg: 'HS256', typ: 'JWT'})
+        .setExpirationTime(exp)
+        .setIssuedAt(iat)
+        .setNotBefore(iat)
+        .sign(new TextEncoder().encode(secret_key));
+    
+    return token;
+  } catch (error) {
+    console.log(error);
+    return '';
+  }
+}
+
+export async function verify(token: string):Promise<jwt.JwtPayload | null> {
+  try {
+    const secret_key = process.env.SECRET_KEY;
+    const { payload } = await jwtVerify(token, new TextEncoder().encode(secret_key));
+    return payload as JwtPayload;
   } catch (error) {
     console.log(error);
     return null;
