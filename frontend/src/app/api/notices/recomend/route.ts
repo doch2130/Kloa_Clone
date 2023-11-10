2519507491033850b04a9b0a77ad21214d3be7fb
doch2130 <doch2130@gmail.com>
2023-11-10 16:58:37 +0900

    feat: notice recomend api modify

--- frontend/src/app/api/notices/recomend/route.ts
+++ frontend/src/app/api/notices/recomend/route.ts
@@ -1,36 +1,38 @@
 import { NextRequest, NextResponse } from "next/server";
+import prisma from '@/app/lib/prisma'
 
 export async function PATCH(req: NextRequest) {
   const body = await req.json();
   // console.log('body ', body);
+
   try {
-    const response = await fetch(`http://localhost:9999/mainNotices?id=${body.postId}`);
-    const data = await response.json();
-
-    // console.log('data ', data);
-
-    if (data.length > 0) {
-      const updateRecomendCount = data[0].recomendCount + 1;
-      const existingDataId = data[0].id;
-      const patchResponse = await fetch(`http://localhost:9999/mainNotices/${existingDataId}`, {
-        method: 'PATCH',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          recomendCount: updateRecomendCount
-        })
-      });
-
-      // console.log('patchResponse.status ', patchResponse.status);
-      if (patchResponse.status === 200) {
-        return new NextResponse(JSON.stringify({ success: true, status: 200, recomendCount: updateRecomendCount }));
-      } else {
-        return new NextResponse(JSON.stringify({ success: true, status: 404 }));
+    const post = await prisma.mainnotices.findFirst({
+      where: {
+        id: body.postId
       }
+    });
+
+    if(!post) {
+      return new NextResponse(JSON.stringify({ success: true, status: 404 }));
+    }
+
+    const updateRecomendCount = post.recomendCount + 1;
+
+    const postUpdate = await prisma.mainnotices.update({
+      data: {
+        recomendCount: updateRecomendCount
+      },
+      where: {
+        id: body.postId
+      }
+    });
+
+    if(postUpdate) {
+      return new NextResponse(JSON.stringify({ success: true, status: 200, recomendCount: updateRecomendCount }));
     } else {
       return new NextResponse(JSON.stringify({ success: true, status: 404 }));
     }
+    
   } catch (err) {
     console.log('err', err);
     return new NextResponse(JSON.stringify({ success: false, status: 500 }));
