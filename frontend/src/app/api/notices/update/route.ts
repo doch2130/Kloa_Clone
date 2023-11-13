54d0f5bfcae9c7e4a2c7cfd305e8dfd31e9e03e0
doch2130 <doch2130@gmail.com>
2023-11-13 15:08:44 +0900

    feat: update api route function modify

--- frontend/src/app/api/notices/update/route.ts
+++ frontend/src/app/api/notices/update/route.ts
@@ -1,37 +1,54 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server"
+import { verifyStringJwt } from "@/app/lib/jwt";
+import prisma from '@/app/lib/prisma'
 
 export async function PATCH(req: NextRequest) {
   const body = await req.json();
   // console.log('body ', body);
   try {
-    const response = await fetch(`http://localhost:9999/mainNotices?id=${body.id}`);
-    const data = await response.json();
+    const accessToken = req.headers.get('Authorization');
+    // console.log('accessToken ', accessToken);
 
-    // console.log('data ', data);
+    if(accessToken === null) {
+      return new NextResponse(JSON.stringify({redirect: true, status: 401}));
+    }
 
-    if (data.length > 0) {
-      const existingDataId = data[0].id;
-      const patchResponse = await fetch(`http://localhost:9999/mainNotices/${existingDataId}`, {
-        method: 'PATCH',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
+    if (!(await verifyStringJwt(accessToken?.slice(7)))) {
+      // 인증 만료, 로그인 재요청
+      req.cookies.delete("next-auth.session-token");
+      return new NextResponse(JSON.stringify({redirect: true, status: 401}));
+    }
+
+    const findPost = await prisma.mainnotices.findMany({
+      where: {
+        id: Number(body.id)
+      },
+      take: 1,
+    });
+
+    if(findPost) {
+      const updatePost = await prisma.mainnotices.update({
+        data: {
           category: body.category,
           title: body.title,
           content: body.content,
-        })
+        },
+        where: {
+          id: Number(body.id)
+        }
       });
 
-      // console.log('patchResponse.status ', patchResponse.status);
-      if (patchResponse.status === 200) {
+      // console.log('updatePost ', updatePost);
+
+      if(updatePost) {
         return new NextResponse(JSON.stringify({ success: true, status: 200 }));
       } else {
         return new NextResponse(JSON.stringify({ success: true, status: 404 }));
       }
     } else {
-      return new NextResponse(JSON.stringify({ success: true, status: 404 }));
+      return new NextResponse(JSON.stringify({ success: true, status: 404, result: [] }));
     }
+
   } catch (err) {
     console.log('err', err);
     return new NextResponse(JSON.stringify({ success: false, status: 500 }));
