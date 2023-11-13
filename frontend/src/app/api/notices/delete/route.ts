9aec875ff906cb78bde32373fb4d96f5ca0330fc
HyoHyun_Home <doch2130@gmail.com>
2023-11-14 01:31:34 +0900

    feat: notice delete api modify

--- frontend/src/app/api/notices/delete/route.ts
+++ frontend/src/app/api/notices/delete/route.ts
@@ -1,10 +1,24 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server"
+import { verifyStringJwt } from "@/app/lib/jwt"
 import prisma from '@/app/lib/prisma'
 
 export async function DELETE(req: NextRequest) {
   const body = await req.json();
   // console.log('body ', body);
   try {
+    const accessToken = req.headers.get('Authorization');
+    // console.log('accessToken ', accessToken);
+
+    if(accessToken === null) {
+      return new NextResponse(JSON.stringify({redirect: true, status: 401}));
+    }
+
+    if (!(await verifyStringJwt(accessToken?.slice(7)))) {
+      // 인증 만료, 로그인 재요청
+      req.cookies.delete("next-auth.session-token");
+      return new NextResponse(JSON.stringify({redirect: true, status: 401}));
+    }
+    
     const post = await prisma.mainnotices.findFirst({
       where: {
         id: body.id
