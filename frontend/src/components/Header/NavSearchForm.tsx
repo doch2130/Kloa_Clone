512d35e2ad575edbec150a5c7b107c82024a7f8a
doch2130 <doch2130@gmail.com>
2023-11-13 17:14:07 +0900

    feat: header nav searchform input event add

--- frontend/src/components/Header/NavSearchForm.tsx
+++ frontend/src/components/Header/NavSearchForm.tsx
@@ -1,12 +1,19 @@
 'use client'
-import React from 'react'
+import React, { useRef, useState } from 'react'
 
 export default function NavSearchForm() {
-  const textChange = () => {
+  const searchValueRef = useRef<HTMLInputElement>(null);
+  const [searchValue, setSearchValue] = useState('');
+  
+  const textChange = (e:React.ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(e.target.value);
   }
+  
   return (
     <form style={{display: 'inline-block'}}>
-      <input id='characterName' type='text' placeholder='캐릭터명을 입력하세요' maxLength={12} value='' onChange={() => textChange}/>
+      <input id='characterName' ref={searchValueRef} className='bg-transparent dark:text-white placeholder:dark:text-[#656770]'
+      type='text' placeholder='캐릭터명을 입력하세요' maxLength={12}
+      onChange={(e) => textChange(e)} value={searchValue}/>
     </form>
   )
 }
