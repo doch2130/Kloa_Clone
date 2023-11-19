7a94da696031b864598ab695497cf883e90075f3
HyoHyun_Home <doch2130@gmail.com>
2023-11-20 00:11:31 +0900

    feat: Header Search Form UI add

--- frontend/src/components/Header/HeaderSearchForm.tsx
+++ frontend/src/components/Header/HeaderSearchForm.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useCallback, useEffect, useRef, useState } from 'react'
 import Image from 'next/image'
 
 import SearchIcon from '@/assets/Icon/search.svg'
@@ -16,28 +16,50 @@ export default function HeaderSearchForm() {
   // 최근검색, 즐겨찾기
   const [popOverCategory, setPopOverCategory] = useState<string>('최근검색');
   
+  // 인풋 창 데이터 변환, 값에 따른 팝업 창 출력 상태
   const textChange = (e:React.ChangeEvent<HTMLInputElement>) => {
+    if(e.target.value !== '') {
+      setIsInputFocus(false);
+    } else {
+      setIsInputFocus(true);
+    }
     setSearchValue(e.target.value);
-  }
-
-  const popOverCategoryChangeHandler = (category:string) => {
-    setPopOverCategory(category);
     return ;
   }
 
-  const popOverOpenEvent = () => {
-    if(searchValue !== '') {
-      return ;
-    }
-    console.log('popover');
-    setIsInputFocus(true);
-  }
+  // 최근검색, 즐겨찾기 전환 함수
+  const popOverCategoryChangeHandler = useCallback((category:string) => {
+    setPopOverCategory(category);
+    return ;
+  }, []);
 
-  const popOverCloseEvent = (e:any) => {
-    console.log('e.currentTarget.contains ', e.target);
-    console.log('searchWrap ', searchWrap.current?.contains(e.target));
-    // setIsInputFocus(false);
-  }
+  // 팝업 창 여는 함수
+  const popOverOpenEvent = useCallback(() => {
+    if(!isInputFocus) {
+      setIsInputFocus(true);
+    }
+    return ;
+  }, [isInputFocus]);
+
+  
+
+  useEffect(() => {
+    // 팝업 창 닫는 함수
+    const popOverCloseEvent = (e:any) => {
+      if(searchWrap.current?.contains(e.target)) {
+        setIsInputFocus(true);
+      } else {
+        setIsInputFocus(false);
+      }
+    }
+    // 팝업 창 닫는 함수 등록
+    document.addEventListener('mousedown', popOverCloseEvent);
+
+    // 언마운트 등록
+    return () => {
+      document.removeEventListener('mousedown', popOverCloseEvent);
+    }
+  }, []);
   
   return (
     <div ref={searchWrap} className='nav-search-wrap'>
@@ -46,16 +68,16 @@ export default function HeaderSearchForm() {
         <form>
           <input id='characterName' ref={searchValueRef} className='bg-transparent dark:text-white placeholder:dark:text-[#656770]'
           type='text' placeholder='캐릭터명을 입력하세요' maxLength={12}
-          onChange={(e) => textChange(e)} value={searchValue} onFocus={() => popOverOpenEvent()}/>
+          onChange={(e) => textChange(e)} value={searchValue} onFocus={() => popOverOpenEvent()} />
         </form>
       </div>
       {isInputFocus &&
       <div className='nav-search-over bg-white dark:bg-[#33353a] rounded-[10px] rounded-t-none border dark:border-[#4d4f55]'>
         <div className='nav-search-over-header'>
-          <button type='button' className={`h-10 dark:bg-[#2c2f33] ${popOverCategory === '최근검색' ? 'dark:text-[#eaf0ec]' : 'nav-search-over-header-unactive'}`}
+          <button type='button' className={`h-10 ${popOverCategory === '최근검색' ? 'dark:text-[#eaf0ec] dark:bg-[#33353a]' : 'nav-search-over-header-unactive dark:bg-[#2c2f33]'}`}
           onClick={() => popOverCategoryChangeHandler('최근검색')}>최근검색</button>
 
-          <button type='button' className={`h-10 dark:bg-[#2c2f33] ${popOverCategory === '즐겨찾기' ? 'dark:text-[#eaf0ec]' : 'nav-search-over-header-unactive'}`}
+          <button type='button' className={`h-10 ${popOverCategory === '즐겨찾기' ? 'dark:text-[#eaf0ec] dark:bg-[#33353a]' : 'nav-search-over-header-unactive dark:bg-[#2c2f33]'}`}
           onClick={() => popOverCategoryChangeHandler('즐겨찾기')}>즐겨찾기</button>
         </div>
         <div className='nav-search-over-body'>
