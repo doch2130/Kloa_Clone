a0de8c2054716857157fb2b2a849ce4ed7133fa0
doch2130 <doch2130@gmail.com>
2023-11-20 14:54:50 +0900

    feat: header search util file create

---
+++ frontend/src/components/Header/HeaderSearchUtil.ts
@@ -0,0 +1,66 @@
+import { Character } from "@/type/characters";
+
+
+/*
+최근 검색 데이터 저장 함수
+@ type recently, favorite
+@ searchValue input data
+@ setStateFunction useStateFunction
+*/
+export const localStorageSaveHandler = (type:string, searchValue:string, setStateFunction:Function) => {
+  if(type === 'recently') {
+    const recentlySearchStorage = localStorage.getItem('recentlySearchStorage');
+    
+    const searchData:Character = {
+      name: searchValue,
+      job: '워로드',
+      icon_url: 'http://aaa.com',
+      level: 60,
+      item_level: 1543,
+      max_item_level: 1543,
+      guild: '명품길드',
+      server: 7
+    }
+    
+    if(recentlySearchStorage !== null) {
+      const recentlySearchStorageJson = JSON.parse(recentlySearchStorage);
+      recentlySearchStorageJson.unshift(searchData);
+      
+      if(recentlySearchStorageJson.length > 5) {
+        recentlySearchStorageJson.pop();
+      }
+
+      localStorage.setItem('recentlySearchStorage', JSON.stringify(recentlySearchStorageJson));
+      setStateFunction(recentlySearchStorageJson);
+    } else {
+      const recentlySearchDataArray = [];
+      recentlySearchDataArray.push(searchData);
+      localStorage.setItem('recentlySearchStorage', JSON.stringify(recentlySearchDataArray));
+      setStateFunction(recentlySearchDataArray);
+    }
+  }
+
+  return ;
+}
+
+
+/*
+검색 데이터 삭제 함수
+@ type recently, favorite
+@ characterName delete name
+@ stateData useState
+@ setStateFunction useStateFunction
+*/
+export const deleteSearchDataHandler = (type:string, characterName:string, stateData:Character[], setStateFunction:Function) => {
+  const filterData = stateData.filter((item:Character) => item.name !== characterName);
+
+  if(type === 'recently') {
+    localStorage.setItem('recentlySearchStorage', JSON.stringify(filterData));
+  } else {
+    localStorage.setItem('favoriteCharactersStorage', JSON.stringify(filterData));
+  }
+
+  setStateFunction(filterData);
+
+  return ;
+}
