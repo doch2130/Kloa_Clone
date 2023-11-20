596e5c80168546f5be74fc88e24630e568ff2bf0
doch2130 <doch2130@gmail.com>
2023-11-20 14:54:10 +0900

    feat: characters type file add

---
+++ frontend/src/type/characters.ts
@@ -0,0 +1,10 @@
+export interface Character {
+  name: string;
+  job: string;
+  icon_url: string;
+  level: number;
+  item_level: number;
+  max_item_level: number;
+  guild: string;
+  server: number;
+}
