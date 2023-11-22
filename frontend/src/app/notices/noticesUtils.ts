b4d4e00ac2b82a2a4a93d619611283cac0e0a274
HyoHyun_Home <doch2130@gmail.com>
2023-11-22 21:37:49 +0900

    feat: noticesUtil file recomend function modify

--- frontend/src/app/notices/noticesUtils.ts
+++ frontend/src/app/notices/noticesUtils.ts
@@ -29,17 +29,24 @@ export const recomendEventHandler = (postId:number, session:Session | null, post
             setPostData((prev: NoticePost) => ({
               ...prev,
               recomendCount: Number(res.recomendCount)
-            }))
+            }));
           }
         }
-        return ;
+        
+        if(res.status === 401 && res.redirect === true) {
+          // alert('로그인이 만료되었습니다. 로그인 후 다시 시도해주세요');
+          return {status: 401, redirect: true};
+        }
+
+        return {status: 200, redirect: false};
       })
       .catch(error => {
-        alert('에러가 발생하였습니다. 새로 고침 후 다시 시도해주세요');
-        return ;
+        // alert('에러가 발생하였습니다. 새로 고침 후 다시 시도해주세요');
+        return {status: 500, redirect: false};
       })
   } else {
-    alert('로그인 후 추천 가능합니다');
-    return ;
+    // alert('로그인 후 추천 가능합니다');
+    return {status: 401, redirect: false};
   }
+  return {status: 500, redirect: false};
 }
