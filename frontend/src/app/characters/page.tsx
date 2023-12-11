78b7bab850e832d65a88e402049d68b3047e6d1c
doch2130 <doch2130@gmail.com>
2023-12-11 16:59:38 +0900

    characters page working

--- frontend/src/app/characters/page.tsx
+++ frontend/src/app/characters/page.tsx
@@ -1,7 +1,19 @@
 import React from 'react'
 
+import styled from './Characters.module.css'
+
+import TopSection from './TopSection'
+
+
 export default function Characters() {
   return (
-    <div>Characters</div>
+    <div className={`${styled.body}`}>
+      <div className={`${styled.topThreeSection}`}>
+        <TopSection />
+      </div>
+      <div className='list'>
+        characters list
+      </div>
+    </div>
   )
 }
