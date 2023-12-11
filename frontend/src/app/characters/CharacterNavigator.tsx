0cf97a0d932c61584c43ee8d142acc0b201d403c
doch2130 <doch2130@gmail.com>
2023-12-11 18:46:19 +0900

    character navigator component modify

--- frontend/src/app/characters/CharacterNavigator.tsx
+++ frontend/src/app/characters/CharacterNavigator.tsx
@@ -1,11 +1,16 @@
+'use clinet'
 import React from 'react'
-import Link from 'next/link'
 
 export default function CharacterNavigator() {
   return (
     <div className='w-full h-[54px] bg-white dark:bg-[#33353a] dark:border-0 border-2 border-lightGrey rounded-[10px] flex justify-between items-center px-[10px] mt-[28px]'>
       {/* text-[#7d8395] */}
-      CharacterNavigator
+      <div className='flex items-center space-x-5'>
+        asd
+      </div>
+      <div className='flex items-center space-x-5'>
+        level option
+      </div>
     </div>
   )
 }
