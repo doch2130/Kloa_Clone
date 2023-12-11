0d660f796d659ef24460f1dc0b851b4e92410dde
doch2130 <doch2130@gmail.com>
2023-12-11 18:19:14 +0900

    character Navigator component modify

--- frontend/src/app/characters/CharacterNavigator.tsx
+++ frontend/src/app/characters/CharacterNavigator.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import Link from 'next/link'
 
 export default function CharacterNavigator() {
   return (
-    <div className='w-full h-[54px] bg-white dark:bg-[#33353a] rounded-[10px] mt-7 flex justify-between items-center px-[10px]'>
+    <div className='w-full h-[54px] bg-white dark:bg-[#33353a] dark:border-0 border-2 border-lightGrey rounded-[10px] flex justify-between items-center px-[10px] mt-[28px]'>
+      {/* text-[#7d8395] */}
       CharacterNavigator
     </div>
   )
