01885cbc9f243fd12bb88f8008e4f86eb756fc1c
doch2130 <doch2130@gmail.com>
2023-12-15 18:15:35 +0900

    import component modify

--- frontend/src/app/characters/CharacterNavigator.tsx
+++ frontend/src/app/characters/CharacterNavigator.tsx
@@ -3,8 +3,7 @@ import React from 'react'
 import downArrow from '@/assets/Icon/downArrow.svg'
 import Image from 'next/image'
 
-// simport ListBoxSelect from '@/components/UI/ListBoxSelect'
-import ListBoxSelectCopy from '@/components/UI/ListBoxSelectCopy'
+import ListBoxSelect from '@/components/UI/ListBoxSelect'
 
 import { serverList } from '@/data/ServerListData'
 import { characterJobList } from '@/data/CharacterJobData'
@@ -18,11 +17,7 @@ export default function CharacterNavigator() {
     <div className='w-full h-[54px] bg-white dark:bg-[#33353a] dark:border-0 border-2 border-lightGrey rounded-[10px] flex justify-between items-center px-[10px] mt-[28px]'>
       <div className='flex items-center space-x-5'>
         <div className='relative w-[150px] h-[38px]'>
-          {/* <ListBoxSelect /> */}
-          <ListBoxSelectCopy
-            classData={{ buttonClass }}
-            listData={serverList}
-          />
+          <ListBoxSelect classData={{ buttonClass }} listData={serverList} />
         </div>
         <div className='relative w-[150px] h-[38px]'>
           <button type='button' className='w-full h-full border-2 border-basicGrey dark:border-[#4d4f55] rounded-[10px] flex justify-between items-center text-placeholder'>
