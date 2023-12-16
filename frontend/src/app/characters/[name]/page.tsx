9b8a3408a035c2e7a481740ae42c90dc77a5bef0
HyoHyun_Home <doch2130@gmail.com>
2023-12-17 00:36:41 +0900

    characters detail page api connect

--- frontend/src/app/characters/[name]/page.tsx
+++ frontend/src/app/characters/[name]/page.tsx
@@ -1,7 +1,56 @@
-import React from 'react'
+'use client'
+import React, { useEffect } from 'react'
+import { useParams } from 'next/navigation';
+import { localStorageSaveHandler } from '../../../components/Header/HeaderSearchUtil'
 
-export default function page() {
+export default function CharacterDetail() {
+  const params = useParams();
+  // const name = params.name;
+  const name = Array.isArray(params.name) ? params.name.join(',') : params.name;
+
+  // 여기서는 데이터 호출하지 않고 페이지만 넘겨준다.
+  // 페이지를 넘겨준 후 API를 호출해서 데이터가 있으면 로컬스토리지 저장도 같이 한다.
+  // 여기서 1번하면 페이지 이동 후 또 해야하는 현상이 생기기 때문에 여기서는 하지 않는다.
+
+  const dataLoadHandler = async (name:string) => {
+    const characterName = decodeURIComponent(name.trim());
+    try {
+      const resposneCharacter = await fetch(`/api/lostark/characters?characterName=${characterName}`, {
+        method: 'get'
+      });
+  
+      if(resposneCharacter.ok) {
+        const result = await resposneCharacter.json();
+  
+        if(result.status === 200) {
+          // 최근 검색 로컬 스토리지 저장
+          // localStorageSaveHandler('recently', searchValueRef.current.value, setRecentlyData);
+          console.log('result ', result);
+        } else if(result.status === 404) {
+          console.log('not found character');
+        } else {
+          alert('캐릭터 검색 중 에러가 발생하였습니다. 잠시 후 다시 시도 해주세요.');
+        }
+        return ;
+  
+      } else {
+        throw new Error('Network response was not ok');
+      }
+    } catch (error) {
+      alert('캐릭터 검색 중 에러가 발생하였습니다. 잠시 후 다시 시도 해주세요.');
+      return ;
+    }
+  }
+
+  useEffect(() => {
+    // dataLoadHandler(name);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
-    <div>Name</div>
+    <>
+    <div>Name: {decodeURIComponent(name)}</div>
+    <div>Name: {name}</div>
+    </>
   )
 }
