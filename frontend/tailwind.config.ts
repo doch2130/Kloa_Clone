6869a3a9c2f7363fc6669b2f1d3dc45825504d57
HyoHyun_Home <doch2130@gmail.com>
2023-12-26 17:13:45 +0900

    tailwind config, responsive setting add

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -15,6 +15,20 @@ const config: Config = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
+    screens: {
+      // 'sm': '576px',
+      // => @media (min-width: 576px) { ... }
+      'sm': {'max': '500px'},
+      // max-width: 500px
+      'smlg': {'min': '500px', 'max': '1069px'},
+      // min-width: 500px and max-width: 1069px
+      'md': '960px',
+      // => @media (min-width: 960px) { ... }
+      'mlg': {'max': '1069px'},
+      // max-width: 1069px
+      'lg': '1440px',
+      // => @media (min-width: 1440px) { ... }
+    },
   },
   plugins: [],
 }
