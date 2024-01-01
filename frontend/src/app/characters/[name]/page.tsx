a9b14237e19c0c51c65251a91a2609cc2342a0af
HyoHyun_Home <doch2130@gmail.com>
2024-01-02 00:39:06 +0900

    react query loading add

--- frontend/src/app/characters/[name]/page.tsx
+++ frontend/src/app/characters/[name]/page.tsx
@@ -17,8 +17,16 @@ export default function CharacterDetail() {
   // 여기서는 데이터 호출하지 않고 페이지만 넘겨준다.
   // 페이지를 넘겨준 후 API를 호출해서 데이터가 있으면 로컬스토리지 저장도 같이 한다.
   // 여기서 1번하면 페이지 이동 후 또 해야하는 현상이 생기기 때문에 여기서는 하지 않는다.
-  const { data } = useQuery({ queryKey: ['character', name], queryFn: () => getCharacter(name) });
+  const { data, isLoading } = useQuery({ queryKey: ['character', name], queryFn: () => getCharacter(name) });
   console.log('data ', data);
+
+  if(isLoading) {
+    return (
+      <div className='w-full h-full absolute z-[150] flex items-center justify-center opacity-[0.9]'>
+        <h1 className='text-5xl font-bold'>Loading...</h1>
+      </div>
+    )
+  }
 
   return (
     <div className='w-full min-h-full bg-lightGrey dark:bg-[#2b2d31] text-[#353945] dark:text-[#eaf0ec] min-[1260px]:flex min-[1260px]:justify-center relative'>
