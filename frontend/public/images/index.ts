0d004efa4220e2a9f0a9b2152233bf7db7db5f25
HyoHyun_Home <doch2130@gmail.com>
2024-01-07 17:34:08 +0900

    public folder image add

---
+++ frontend/public/images/index.ts
@@ -0,0 +1,137 @@
+import ImgSonabelArt from './Art/SonabelArt.webp';
+import ImgNaverBtnDark from './Auth/Naver/btnD_완성형.png';
+import ImgNaverBtnDefault from './Auth/Naver/btnG_완성형.png';
+import ImgCardAwake from './Card/imgCardAwake.webp';
+import ImgCardGrade from './Card/imgCardGrade.webp';
+import ImgCharacterImage1 from './Character/characterImage1.png';
+import ImgCharacterImage2 from './Character/characterImage2.png';
+import ImgCharacterImage3 from './Character/characterImage3.png';
+import ImgBattleArenaOff from './Icon/battleArena_off.png';
+import ImgBattleArenaOn from './Icon/battleArena_on.png';
+import ImgBossOff from './Icon/boss_off.png';
+import ImgBossOn from './Icon/boss_on.png';
+import ImgChaosGateOff from './Icon/chaosGate_off.png';
+import ImgChaosGateOn from './Icon/chaosGate_on.png';
+import ImgGhostShipOff from './Icon/ghostShip_off.png';
+import ImgGhostShipOn from './Icon/ghostShip_on.png';
+import Imgkloa1 from './Logo/kloa1.webp';
+import Imgkloa2 from './Logo/kloa2.webp';
+import ImgAuctionEmpty2 from './Mococo/img_auction_empty2.png';
+import ImgMainImage1 from './SlideImage/main_image_1.webp';
+import ImgMainImage2 from './SlideImage/main_image_2.webp';
+import ImgMainImage3 from './SlideImage/main_image_3.webp';
+import ImgMainImage4 from './SlideImage/main_image_4.webp';
+
+
+import IconIslandCoinbox from './Item/ico_island_coinbox.png'
+import IconIslandCoin1 from './Item/ico_island_coin1.png'
+import IconIslandCoin2 from './Item/ico_island_coin2.png'
+import IconIslandSilver from './Item/ico_island_silver.png'
+import IconIslandGold from './Item/ico_island_gold.png'
+import IconIslandHeart from './Item/ico_island_heart.png'
+import IconIslandCardpack from './Item/ico_island_cardpack.png'
+import IconIslandExp from './Item/ico_island_exp.png'
+import IconIslandCard from './Item/ico_island_card.png'
+import IconIslandGift from './Item/ico_island_gift.png'
+import IconIslandHarmony1 from './Item/ico_island_harmony1.png'
+import IconIslandHarmony2 from './Item/ico_island_harmony2.png'
+import IconIslandHarmony3 from './Item/ico_island_harmony3.png'
+import IconIslandLushreeds from './Item/ico_island_lushreeds.png'
+import IconIslandMap from './Item/ico_island_map.png'
+import IconIslandMarionette from './Item/ico_island_marionette.png'
+import IconIslandMazebox from './Item/ico_island_mazebox.png'
+import IconIslandMbox from './Item/ico_island_mbox.png'
+import IconIslandMbutterfly from './Item/ico_island_mbutterfly.png'
+import IconIslandMdead from './Item/ico_island_mdead.png'
+import IconIslandMedeia from './Item/ico_island_medeia.png'
+import IconIslandMonte from './Item/ico_island_monte.png'
+import IconIslandPocket from './Item/ico_island_pocket.png'
+import IconIslandRabbit from './Item/ico_island_rabbit.png'
+import IconIslandRune1 from './Item/ico_island_rune1.png'
+import IconIslandRune2 from './Item/ico_island_rune2.png'
+import IconIslandRune3 from './Item/ico_island_rune3.png'
+import IconIslandSimbol from './Item/ico_island_simbol.png'
+import IconIslandSnowbox from './Item/ico_island_snowbox.png'
+import IconIslandSong1 from './Item/ico_island_song1.png'
+import IconIslandSong2 from './Item/ico_island_song2.png'
+import IconIslandSuras from './Item/ico_island_suras.png'
+import IconIslandVolare from './Item/ico_island_volare.png'
+import IconIslandWolf from './Item/ico_island_wolf.png'
+import IconIslandLylaibox from './Item/ico_island_lylaibox.png'
+import IconIslandLylaistatue1 from './Item/ico_island_lylaistatue1.png'
+import IconIslandLylaistatue2 from './Item/ico_island_lylaistatue2.png'
+import IconIslandLylaistatue3 from './Item/ico_island_lylaistatue3.png'
+import IconIslandLylaitoy1 from './Item/ico_island_lylaitoy1.png'
+import IconIslandLylaitoy2 from './Item/ico_island_lylaitoy2.png'
+import IconIslandLylaitoy3 from './Item/ico_island_lylaitoy3.png'
+
+
+export const islandIconList = {
+  IconIslandCoinbox,
+  IconIslandCoin1,
+  IconIslandCoin2,
+  IconIslandSilver,
+  IconIslandGold,
+  IconIslandHeart,
+  IconIslandCardpack,
+  IconIslandExp,
+  IconIslandCard,
+  IconIslandGift,
+  IconIslandHarmony1,
+  IconIslandHarmony2,
+  IconIslandHarmony3,
+  IconIslandLushreeds,
+  IconIslandMap,
+  IconIslandMarionette,
+  IconIslandMazebox,
+  IconIslandMbox,
+  IconIslandMbutterfly,
+  IconIslandMdead,
+  IconIslandMedeia,
+  IconIslandMonte,
+  IconIslandPocket,
+  IconIslandRabbit,
+  IconIslandRune1,
+  IconIslandRune2,
+  IconIslandRune3,
+  IconIslandSimbol,
+  IconIslandSnowbox,
+  IconIslandSong1,
+  IconIslandSong2,
+  IconIslandSuras,
+  IconIslandVolare,
+  IconIslandWolf,
+  IconIslandLylaibox,
+  IconIslandLylaistatue1,
+  IconIslandLylaistatue2,
+  IconIslandLylaistatue3,
+  IconIslandLylaitoy1,
+  IconIslandLylaitoy2,
+  IconIslandLylaitoy3
+}
+
+export {
+  ImgSonabelArt,
+  ImgNaverBtnDark,
+  ImgNaverBtnDefault,
+  ImgCardAwake,
+  ImgCardGrade,
+  ImgCharacterImage1,
+  ImgCharacterImage2,
+  ImgCharacterImage3,
+  ImgBattleArenaOff,
+  ImgBattleArenaOn,
+  ImgBossOff,
+  ImgBossOn,
+  ImgChaosGateOff,
+  ImgChaosGateOn,
+  ImgGhostShipOff,
+  ImgGhostShipOn,
+  Imgkloa1,
+  Imgkloa2,
+  ImgAuctionEmpty2,
+  ImgMainImage1,
+  ImgMainImage2,
+  ImgMainImage3,
+  ImgMainImage4,
+};
---
+++ frontend/public/svgs/index.ts
@@ -0,0 +1,52 @@
+import IconAlert from './Icon/alertIcon.svg';
+import IconClose from './Icon/close.svg';
+import IconDiscord from './Icon/discord.svg';
+import IconDownArrow from './Icon/downArrow.svg';
+import IconElixir from './Icon/elixir.svg';
+import IconEye from './Icon/eye.svg';
+import IconLeftArrow from './Icon/leftArrow.svg';
+import IconLeftDoubleArrow from './Icon/leftDoubleArrow.svg';
+import IconList from './Icon/list.svg';
+import IconMococo from './Icon/Mococo.svg';
+import IconMoon from './Icon/Moon.svg';
+import IconRightArrow from './Icon/RightArrow.svg';
+import IconRightDoubleArrow from './Icon/RightDoubleArrow.svg';
+import IconSearch from './Icon/Search.svg';
+import IconSlideLeftArrow from './Icon/SlideLeftArrow.svg';
+import IconSlideRightArrow from './Icon/SlideRightArrow.svg';
+import IconStarEmpty from './Icon/StarEmpty.svg';
+import IconStarFull from './Icon/StarFull.svg';
+import IconSun from './Icon/Sun.svg';
+import IconTranscendance from './Icon/Transcendance.svg';
+import IconUpArrow from './Icon/UpArrow.svg';
+
+import IconOne from './Number/one.svg';
+import IconTwo from './Number/two.svg';
+import IconThree from './Number/three.svg';
+
+export {
+  IconAlert,
+  IconClose,
+  IconDiscord,
+  IconDownArrow,
+  IconElixir,
+  IconEye,
+  IconLeftArrow,
+  IconLeftDoubleArrow,
+  IconList,
+  IconMococo,
+  IconMoon,
+  IconRightArrow,
+  IconRightDoubleArrow,
+  IconSearch,
+  IconSlideLeftArrow,
+  IconSlideRightArrow,
+  IconStarEmpty,
+  IconStarFull,
+  IconSun,
+  IconTranscendance,
+  IconUpArrow,
+  IconOne,
+  IconTwo,
+  IconThree
+};
