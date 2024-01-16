c8f3aa0a1e13912a196fd2981a08705fb3336656
doch2130 <doch2130@gmail.com>
2024-01-16 18:30:16 +0900

    guild api folder location change

--- frontend/src/components/Character/Detail/GuildTab/utils.ts
+++ frontend/src/components/Character/Detail/GuildTab/utils.ts
@@ -2,7 +2,7 @@ import { guildResponseType } from "@/types/guild";
 
 export async function getGuildInfo(guildName:string):Promise<guildResponseType> {
   // console.log('guildName ', guildName);
-  const response = await fetch(`/api/lostark/guilds?guildName=${guildName}`);
+  const response = await fetch(`/api/guild?guildName=${guildName}`);
   const guildInfo = (await response.json()) as guildResponseType;
   return guildInfo;
 }
