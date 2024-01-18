8848b0126ab0d411270ae80f3e8a392b9ca9f278
HyoHyun_Home <doch2130@gmail.com>
2024-01-18 17:14:51 +0900

    ownedcharactertab, gold check section complete

--- frontend/src/components/Character/Detail/OwnedCharacterTab/OwnedCharacterTab.tsx
+++ frontend/src/components/Character/Detail/OwnedCharacterTab/OwnedCharacterTab.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { Fragment, useEffect, useState } from 'react'
+import React, { ChangeEvent, Fragment, useEffect, useState } from 'react'
 import { useParams } from 'next/navigation'
 import Link from 'next/link'
 import Image from 'next/image'
@@ -8,20 +8,103 @@ import { Disclosure } from '@headlessui/react'
 
 import { getOwnedCharacter, itemLevelBorderStyleFunction } from '../utils'
 
-import { dungeonLevelList } from '@/data/DungeonData'
+import { dungeonGoldList, dungeonLevelList } from '@/data/DungeonData'
 
 import { CharacterInfo } from '@/types/siblings'
 
 import { characterJobIconList } from '/public/images'
 import UpArrowSvg from '@/components/UI/UpArrowSvg'
-import GoldSelect from './GoldSelect'
+
+interface characterDungeonListResultType {
+  name: string
+  itemLevel: string
+  totalGold: number
+  goldDungeon: string[]
+}
+
+const characterDungeonList = (goldCharacterList:CharacterInfo[]) => {
+  const result:characterDungeonListResultType[] = [];
+
+  goldCharacterList?.forEach((character) => {
+    const itemLevel = Number(character.ItemAvgLevel.replace(/,/g, ''));
+    const characterDungeon = dungeonLevelList(itemLevel);
+    const goldList: string[] = [];
+
+    characterDungeon?.some((list:string) => {
+      if(list !== '카멘 하드 4' && list !== '아브렐슈드 하드 4' && list !== '아브렐슈드 노말 4' && goldList.length < 3) {
+
+        if(itemLevel >= 1600 && list === '카양겔 하드') {
+          // 카양겔 하드 패스, 아브렐슈드 1~4 넣기
+          // some에서 false는 continue
+          return false;
+        }
+
+        if(itemLevel >= 1600 && list === '아브렐슈드 하드 1~3') {
+          // 카양겔 하드 패스, 아브렐슈드 1~4 넣기
+          goldList.push(list);
+          return false;
+        }
+
+        goldList.push(list);
+
+      }
+    });
+
+    if(goldList.includes('카멘 하드 3')) {
+      goldList.unshift('카멘 하드 4');
+    }
+
+    if((goldList.includes('아브렐슈드 하드 3') || goldList.includes('아브렐슈드 하드 1~3')) && itemLevel >= 1560) {
+      goldList.push('아브렐슈드 하드 4');
+    }
+
+    if((goldList.includes('아브렐슈드 하드 3') || goldList.includes('아브렐슈드 하드 1~3')) && itemLevel < 1560) {
+      goldList.push('아브렐슈드 노말 4');
+    }
+
+    if((goldList.includes('아브렐슈드 노말 3') || goldList.includes('아브렐슈드 노말 1~3')) && itemLevel >= 1520) {
+      goldList.push('아브렐슈드 노말 4');
+    }
+
+    // console.log('character ', character.CharacterName);
+    // console.log('characterDungeon ', characterDungeon);
+    // console.log('goldList ', goldList);
+
+    let total = 0;
+    goldList?.forEach((dungeon) => {
+      total += dungeonGoldList[dungeon];
+    })
+
+    const updateCharacterGold = {
+      'name': character.CharacterName,
+      'itemLevel': character.ItemAvgLevel,
+      'totalGold': total,
+      'goldDungeon': goldList
+    }
+
+    result.push(updateCharacterGold);
+
+  });
+
+  return result;
+}
+
+const goldTotalCalculator = (lastGoldCharacterList: characterDungeonListResultType[]) => {
+  let total = 0;
+
+  lastGoldCharacterList?.forEach((list) => {
+    total += list.totalGold;
+  });
+
+  return total;
+}
 
 export default function OwnedCharacterTab() {
   const params = useParams();
   const name = Array.isArray(params.name) ? params.name.join(',') : params.name;
   const [serverList, setServerList] = useState<string[]>([]);
-  const [goldCharacterList, setGoldCharacterList] = useState<CharacterInfo[]>([]);
-  const [totalGoldList, setTotalGoldList] = useState<Number[]>([]);
+  const [totalGold, setTotalGold] = useState<number>(0);
+  const [goldCharacterList, setGoldCharacterList] = useState<characterDungeonListResultType[]>([]);
 
   const { data:haveCharacterList, isLoading:isHavaCharacterListLoading } = useQuery({ queryKey: ['havaCharacterList', name], queryFn: () => getOwnedCharacter(name) });
 
@@ -56,18 +139,64 @@ export default function OwnedCharacterTab() {
           }
         });
 
-        if (sortedFilterGoldCharacterList.length > 6) {
-          const slicedGoldCharacterList = sortedFilterGoldCharacterList.slice(0, 6);
-          setGoldCharacterList(slicedGoldCharacterList);
-        } else {
-          setGoldCharacterList(sortedFilterGoldCharacterList);
-        }
+        const lastGoldCharacterList = characterDungeonList(sortedFilterGoldCharacterList?.slice(0, 6) || []);
+        console.log('lastGoldCharacterList ', lastGoldCharacterList);
+        
+        const updateTotalGold = goldTotalCalculator(lastGoldCharacterList);
+        // console.log('updateTotalGold ', updateTotalGold);
 
+        setGoldCharacterList(lastGoldCharacterList);
+        setTotalGold(updateTotalGold);
+        
       }
 
     }
   }, [haveCharacterList, name]);
+
+
+  useEffect(() => {
+    const updateTotalGold = goldTotalCalculator(goldCharacterList);
+    setTotalGold(updateTotalGold);
+  }, [goldCharacterList]);
   
+
+  const selectDungeonHandler = (e:ChangeEvent<HTMLInputElement>, characterDungeonInfo:characterDungeonListResultType) => {
+    const isChecked = e.target.checked;
+    const value = e.target.value;
+
+    const filterCharacter = goldCharacterList.filter((list) => list.name === characterDungeonInfo.name);
+    const updateFilterCharacter = filterCharacter;
+
+    if(isChecked) {
+      // 체크 안되어 있는 애 클릭하면 실행
+      updateFilterCharacter[0].goldDungeon.push(value);
+      let total = 0;
+      updateFilterCharacter[0].goldDungeon?.forEach((dungeon) => {
+        total += dungeonGoldList[dungeon];
+      })
+      updateFilterCharacter[0].totalGold = total;
+    } else {
+      // 체크되어 있는 애 클릭하면 실행
+      updateFilterCharacter[0].goldDungeon = filterCharacter[0].goldDungeon.filter((dungeonName) => dungeonName !== value);
+      let total = 0;
+      updateFilterCharacter[0].goldDungeon?.forEach((dungeon) => {
+        total += dungeonGoldList[dungeon];
+      })
+      updateFilterCharacter[0].totalGold = total;
+    }
+
+    const updateGoldCharacterList = goldCharacterList.map((list) => {
+      if(list.name === updateFilterCharacter[0].name) {
+        return updateFilterCharacter[0]
+      }
+      return list;
+    });
+    
+    setGoldCharacterList(updateGoldCharacterList);
+  };
+
+  // console.log('goldCharacterList ', goldCharacterList);
+
   if(isHavaCharacterListLoading) {
     return (
       <div className='w-full max-w-[700px] h-full absolute z-[150] flex items-center justify-center opacity-[0.9]'>
@@ -75,10 +204,6 @@ export default function OwnedCharacterTab() {
       </div>
     )
   }
-
-  // console.log('haveCharacterList ', haveCharacterList);
-  // console.log('serverList ', serverList);
-  // console.log('goldCharacterList ', goldCharacterList);
 
   return (
     <>
@@ -90,26 +215,45 @@ export default function OwnedCharacterTab() {
           <Disclosure.Button className='flex items-center justify-between w-full'>
             <p className="text-base font-semibold">주간 골드 획득량</p>
             <div className='flex items-center'>
-              <p className='mr-1.5 text-[#d79412] font-semibold text-base'>{totalGoldList.reduce((acc, currentNumber) => acc.valueOf() + currentNumber.valueOf(), 0).toString()} G</p>
-              <UpArrowSvg isOpen={open} classNameDefault={'w-5 h-5 transition-transform duration-100'} classNameOpen={'rotate-180'} classNameClose={'rotate-0'} />
+              <p className='text-[#d79412] font-semibold text-lg'>{totalGold.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",")} G</p>
+              <UpArrowSvg isOpen={open} classNameDefault={'w-7 h-6 transition-transform duration-100'} classNameOpen={'rotate-180'} classNameClose={'rotate-0'} />
             </div>
           </Disclosure.Button>
           <Disclosure.Panel className="mt-4 transform scale-100 opacity-100">
             <div className='px-[17px] py-4 bg-[#f5f6f7] dark:bg-[#2b2d31] rounded-xl grid grid-cols-2 gap-x-8 gap-y-3'>
-              {goldCharacterList?.map((characterInfo:CharacterInfo, index:number) => {
-                const dungeonList = dungeonLevelList(Number(characterInfo.ItemAvgLevel.replace(/,/g, '')));
+              {goldCharacterList?.map((goldList, index:number) => {
+                const dungeonList = dungeonLevelList(Number(goldList.itemLevel.replace(/,/g, '')));
                 return (
-                  <div key={`${characterInfo.CharacterName}_${index}`}>
+                  <div key={`${goldList.name}_${index}`}>
                     <div className='flex justify-between'>
-                      <p className='text-sm font-semibold'>{characterInfo.CharacterName}</p>
-                      <p className='text-sm font-semibold'>총골드</p>
+                      <p className='text-base font-semibold'>{goldList.name}</p>
+                      <p className='text-base font-semibold'>{goldList.totalGold}G</p>
                     </div>
                     <div className='flex justify-between'>
                       <div>
-                        <p className='text-xs text-[#7d8395]'>{`${parseInt(characterInfo.ItemAvgLevel.replace(/,/g, ''))} ${characterInfo.CharacterClassName}`}</p>
+                        <p className='text-xs text-[#7d8395]'>{`${parseInt(goldList.itemLevel.replace(/,/g, ''))} ${goldList.name}`}</p>
                       </div>
                       <div className='border-l-2 dark:border-l-[#4d4f55] mt-0.5 pl-2'>
-                        <GoldSelect characterName={characterInfo.CharacterName} dungeonList={dungeonList} setTotalGoldList={setTotalGoldList} itemLevel={Number(characterInfo.ItemAvgLevel.replace(/,/g, ''))} />
+                        {dungeonList?.map((dungeon, idx:number) => {
+                          return (
+                            <label key={`${goldList.name}_${dungeon}_${idx}`} className='flex items-center'>
+                              <input type='checkbox' checked={goldList.goldDungeon.includes(dungeon)} value={dungeon} onChange={(e) => selectDungeonHandler(e, goldList)} />
+                              <span className='ml-2 inline-block w-[130px] text-[0.82rem] leading-5 text-[#7d8395]'>{dungeon}
+                                {(dungeon === '카멘 하드 4' || dungeon === '아브렐슈드 하드 4' || dungeon === '아브렐슈드 노말 4') &&
+                                <svg className='inline ml-1 -mt-0.5 w-4 h-4' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" strokeLinejoin="round" strokeLinecap="round" fill="none" stroke="currentColor" strokeWidth="2">
+                                  <g>
+                                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                    <path d="m20,11a8.1,8.1 0 0 0 -15.5,-2m-0.5,-4l0,4l4,0"></path>
+                                    <path d="m18,15l2,0a1,1 0 0 1 1,1l0,1a1,1 0 0 1 -1,1l-1,0a1,1 0 0 0 -1,1l0,1a1,1 0 0 0 1,1l2,0"></path>
+                                    <path d="m4.12232,13.12232c0.59032,4.57859 4.17911,7.52459 9.50764,7.12232"></path>
+                                  </g>
+                                </svg>
+                                }
+                              </span>
+                              <span className='inline-block w-[58px] justify-self-end text-right text-sm font-semibold'>{dungeonGoldList[dungeon]}G</span>
+                            </label>
+                          )
+                        })}
                       </div>
                     </div>
                   </div>
