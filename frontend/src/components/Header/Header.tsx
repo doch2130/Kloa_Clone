6d089033224a21b49eb1e3ccf1f4e18ad212c830
doch2130 <doch2130@gmail.com>
2024-01-26 18:38:15 +0900

    sell page working start

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React from 'react'
+import React, { Fragment } from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
@@ -29,8 +29,16 @@ export default function Header() {
             <Link href='/notices?page=1' as='/notices?page=1' className={`drop-in-underline duration-300 dark:text-[#B9BBBE] dark:hover:text-[#a36bfc] ${pathName.includes('/notices') ? 'text-[#8045dd] dark:text-[#a36bfc] drop-in-underline-active' : 'text-[#7d8395]'}`}>공지사항</Link>
             <Link href='/characters' as='/characters' className={`drop-in-underline duration-300 dark:text-[#B9BBBE] dark:hover:text-[#a36bfc] ${pathName.includes('/characters') ? 'text-[#8045dd] dark:text-[#a36bfc] drop-in-underline-active' : 'text-[#7d8395]'}`}>전투정보실</Link>
             <Link href='/merchant' as='/merchant' className={`drop-in-underline duration-300 dark:text-[#B9BBBE] dark:hover:text-[#a36bfc] ${pathName.includes('/merchant') ? 'text-[#8045dd] dark:text-[#a36bfc] drop-in-underline-active' : 'text-[#7d8395]'}`}>떠돌이 상인</Link>
-            <Link href='/sell' as='/sell' className={`drop-in-underline duration-300 dark:text-[#B9BBBE] dark:hover:text-[#a36bfc] ${pathName.includes('/sell') ? 'text-[#8045dd] dark:text-[#a36bfc] drop-in-underline-active' : 'text-[#7d8395]'}`}>아이템 거래</Link>
+            <Link href='/exchange/sell' as='/exchange/sell' className={`drop-in-underline duration-300 dark:text-[#B9BBBE] dark:hover:text-[#a36bfc] relative group/item ${pathName.includes('/sell') ? 'text-[#8045dd] dark:text-[#a36bfc] drop-in-underline-active' : 'text-[#7d8395]'}`}>
+              아이템 거래
+              <div className='absolute top-[45px] flex items-center flex-col invisible group-hover/item:visible shadow-[0px_1px_4px_0px_rgba(0,0,0,0.25)] w-[110px] text-black dark:text-white rounded-b-md rounded-r-md'>
+                <Link href='/exchange/sell' as='/exchange/sell' className='flex h-[50px] items-center'>아이템 판매</Link>
+                <Link href='/exchange/buy' as='/exchange/buy' className='flex h-[50px] items-center exchangeBuy'>아이템 구매</Link>
+              </div>
+            </Link>
             {/* <Link href='/crow' as='/crow' className={`drop-in-underline duration-300 dark:text-[#B9BBBE] dark:hover:text-[#a36bfc] ${pathName.includes('/crow') ? 'text-[#8045dd] dark:text-[#a36bfc] drop-in-underline-active' : 'text-[#7d8395]'}`}>계산기</Link> */}
+
+            
           </div>
           <HeaderSearchForm />
         </div>
