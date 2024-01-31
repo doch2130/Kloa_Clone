1c19652cf285ce26cc4a37b75b195e9d455c276c
HyoHyun_Home <doch2130@gmail.com>
2024-01-31 10:54:04 +0900

    table add, db setting

---
+++ backend/model/merchant.js
@@ -0,0 +1,44 @@
+const Merchant = (Sequelize, DataTypes) => {
+  return Sequelize.define(
+    'merchant',
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      server: {
+        type: DataTypes.STRING(191),
+        allowNull: false,
+      },
+      npcName: {
+        type: DataTypes.STRING(191),
+        allowNull: true,
+        defaultValue: '',
+      },
+      informant: {
+        type: DataTypes.STRING(191),
+        allowNull: true,
+        defaultValue: '',
+      },
+      itemList: {
+        type: DataTypes.STRING(191),
+        allowNull: true,
+        defaultValue: '',
+      },
+      reportTime: {
+        type: DataTypes.DATE,
+        allowNull: true,
+        defaultValue: Sequelize.NOW,
+      },
+    },
+    {
+      tableName: 'merchant',
+      freezeTableName: true,
+      timestamps: false
+    }
+  );
+};
+
+module.exports = Merchant;
